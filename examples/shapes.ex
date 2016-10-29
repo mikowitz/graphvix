0a0f57c58b916c7429ae6158929245b83fe81abb
Michael Berkowitz <michael.berkowitz@gmail.com>
2016-10-29 12:32:13 -0400

    Update examples with new functions

--- examples/shapes.ex
+++ examples/shapes.ex
@@ -4,13 +4,12 @@ Graph.restart
 Graph.update(size: "4,4")
 
 {a, _} = Node.new(label: "a", shape: "polygon", sides: 5, peripheries: 3, color: "lightblue", style: "filled")
-{b, _} = Node.new(label: "b")
+{b, _} = Node.new("b")
 {c, _} = Node.new(label: "hello world", shape: "polygon", sides: 4, skew: ".4")
 {d, _} = Node.new(label: "d", shape: "invtriangle")
 {e, _} = Node.new(label: "e", shape: "polygon", sides: 4, distortion: ".7")
 
-Edge.new(a, b)
-Edge.new(b, c)
+Edge.chain([a, b, c])
 Edge.new(b, d)
 
 Graph.graph
