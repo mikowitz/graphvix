ad21823e43d813b6f9bc9a4568662d898d73e5db
Michael Berkowitz <michael.berkowitz@gmail.com>
2016-11-17 15:09:11 -0500

    Move new callbacks to Graphvix.Callbacks

--- lib/graphvix/callbacks.ex
+++ lib/graphvix/callbacks.ex
@@ -2,188 +2,106 @@ defmodule Graphvix.Callbacks do
   @moduledoc false
   defmacro __using__(_) do
     quote do
-      @empty_graph %{nodes: %{}, edges: %{}, clusters: %{}, attrs: []}
+      import Graphvix.Graph.Helpers
 
-      ## CAST
+      ## CALLBACKS
 
-      # load
-      def handle_cast({:load, new_graph}, _graph) do
-        {:noreply, new_graph}
+      def init({state_pid, _}) do
+        new_state = {state_pid, Graphvix.State.current_graph(state_pid)}
+        schedule_save()
+        {:ok, new_state}
       end
-      # reset
-      def handle_cast(:reset, _graph) do
-        {:noreply, @empty_graph}
+
+      def handle_call(:current_graph, _from, state={_, graph}) do
+        {:reply, graph, state}
       end
-      # save as txt
-      def handle_cast({:save, filename, :txt}, graph) do
-        File.write(filename <> ".txt", inspect(graph))
-        {:noreply, graph}
+      def handle_call(:ls, _from, state={state_pid, _}) do
+        graph_names = Graphvix.State.ls(state_pid)
+        {:reply, graph_names, state}
       end
-      # save as dot
-      def handle_cast({:save, filename, :dot}, graph) do
-        File.write(filename <> ".dot", Graphvix.Writer.write(graph))
-        {:noreply, graph}
-      end
-      # update element
-      def handle_cast({:update, type, id, attrs}, graph) do
-        new_graph = case find_element(graph, id, type) do
-          nil -> graph
-          element ->
-          case kind_of_element(graph, id) do
-            :node -> update_attrs_for_element_in_graph(graph, id, Map.get(graph.nodes, id), :nodes, attrs)
-            :edge -> update_attrs_for_element_in_graph(graph, id, Map.get(graph.edges, id), :edges, attrs)
-            _ -> graph
-          end
+      def handle_call({:add_node, attrs}, _from, state={state_pid, graph}) do
+        case graph do
+          nil ->
+            {:reply, {:error, :enograph}, state}
+          {name, graph} ->
+            id = get_id
+            new_node = %{ attrs: attrs }
+            new_nodes = Map.put(graph.nodes, id, new_node)
+            new_graph = %{ graph | nodes: new_nodes }
+            {:reply, {id, new_node}, {state_pid, {name, new_graph}}}
         end
-        {:noreply, new_graph}
       end
-      # update graph
-      def handle_cast({:update, attrs}, graph) do
-        new_graph = %{ graph | attrs: merge_without_nils(graph.attrs, attrs) }
-        {:noreply, new_graph}
-      end
-
-      ## CALL
-
-      # find
-      def handle_call({:find, id, type}, _from, graph) do
-        res = case find_element(graph, id, type) do
-          nil -> {:error, {:enotfound, type}}
-          element -> element
-        end
-        {:reply, res, graph}
-      end
-      # delete
-      def handle_call({:delete, id, type}, _from, graph) do
-        {res, new_graph} = case kind_of_element(graph, id) do
-          ^type -> {:ok, remove(type, id, graph)}
-          _ -> {{:error, {:enotfound, type}}, graph}
-        end
-        {:reply, res, new_graph}
-      end
-      # get
-      def handle_call(:get, _from, graph) do
-        {:reply, graph, graph}
-      end
-      # add node
-      def handle_call({:add_node, attrs}, _from, graph) do
-        id = get_id
-        new_node = %{ attrs: attrs }
-        new_nodes = Map.put(graph.nodes, id, new_node)
-        {:reply, {id, new_node}, %{ graph | nodes: new_nodes }}
-      end
-      # add edge
-      def handle_call({:add_edge, start_id, end_id, attrs}, _from, graph) do
+      def handle_call({:add_edge, start_id, end_id, attrs}, _from, {state_pid, {name, graph}}) do
         id = get_id
         new_edge = %{ start_node: start_id, end_node: end_id, attrs: attrs }
         new_edges = Map.put(graph.edges, id, new_edge)
-        {:reply, {id, new_edge}, %{ graph | edges: new_edges }}
+        new_graph = %{ graph | edges: new_edges }
+        {:reply, {id, new_edge}, {state_pid, {name, new_graph}}}
       end
-      # add cluster
-      def handle_call({:add_cluster, node_ids}, _from, graph) do
+      def handle_call({:add_cluster, node_ids}, _from, {state_pid, {name, graph}}) do
         id = get_id
         new_cluster = %{ node_ids: node_ids }
         new_clusters = Map.put(graph.clusters, id, new_cluster)
-        {:reply, {id, new_cluster}, %{ graph | clusters: new_clusters }}
+        new_graph = %{ graph | clusters: new_clusters }
+        {:reply, {id, new_cluster}, {state_pid, {name, new_graph}}}
       end
-      # add to cluster
-      def handle_call({:add_to_cluster, cluster_id, node_ids}, _from, graph) do
+      def handle_call({:add_to_cluster, cluster_id, node_ids}, _from, {state_pid, {name, graph}}) do
         cluster = Map.get(graph.clusters, cluster_id)
         new_node_ids = (cluster.node_ids ++ node_ids) |> Enum.uniq
         new_cluster = %{ cluster | node_ids: new_node_ids }
         new_clusters = %{ graph.clusters | cluster_id => new_cluster }
-        {:reply, new_cluster, %{ graph | clusters: new_clusters }}
+        {:reply, new_cluster, {state_pid, {name, %{ graph | clusters: new_clusters }}}}
       end
-      # remove from cluster
-      def handle_call({:remove_from_cluster, cluster_id, node_ids}, _from, graph) do
+      def handle_call({:remove_from_cluster, cluster_id, node_ids}, _from, {state_pid, {name, graph}}) do
         cluster = Map.get(graph.clusters, cluster_id)
         new_node_ids = (cluster.node_ids -- node_ids) |> Enum.uniq
         new_cluster = %{ cluster | node_ids: new_node_ids }
         new_clusters = %{ graph.clusters | cluster_id => new_cluster }
-        {:reply, new_cluster, %{ graph | clusters: new_clusters }}
+        {:reply, new_cluster, {state_pid, {name, %{ graph | clusters: new_clusters }}}}
       end
-
-      ## PRIVATE
-
-      defp update_attrs_for_element_in_graph(graph, id, element, key, attrs) do
-        new_attrs = merge_without_nils(element.attrs, attrs)
-        new_element = %{ element | attrs: new_attrs }
-        %{ graph | key => %{ Map.get(graph, key) | id => new_element } }
-      end
-
-      defp merge_without_nils(original, changes) do
-        original
-        |> Keyword.merge(changes)
-        |> reject_nils
-      end
-
-      defp reject_nils(coll) do
-        Enum.reject(coll, fn {_, v} -> is_nil(v) end)
-      end
-
-      defp update_graph_with_element_removed(graph, key, id) do
-        with with_removed <- remove_from_map!(Map.get(graph, key), id) do
-          %{ graph | key => with_removed }
+      def handle_call({:find, id, type}, _from, state={_, {_name, graph}}) do
+        res = case find_element(graph, id, type) do
+          nil -> {:error, {:enotfound, type}}
+          element -> element
         end
+        {:reply, res, state}
       end
-
-      defp remove_edges_attached_to_node(graph, node_id) do
-       new_edges = graph.edges
-       |> Enum.reject(&edge_connects_to_node?(&1, node_id))
-       |> Enum.into(Map.new)
-       %{ graph | edges: new_edges }
-      end
-
-      defp edge_connects_to_node?({_, %{ start_node: s_id, end_node: e_id }}, n_id) do
-        s_id == n_id || e_id == n_id
-      end
-
-      def remove_node_from_clusters(graph, node_id) do
-        new_clusters = Enum.map(graph.clusters, fn {id, cluster} ->
-          {id, %{ cluster | node_ids: cluster.node_ids -- [node_id] }}
-        end) |> Enum.into(Map.new)
-        %{ graph | clusters: new_clusters }
-      end
-
-      defp find_element(graph, id) do
-        Map.get(graph.nodes, id) || Map.get(graph.edges, id) || Map.get(graph.clusters, id)
-      end
-      defp find_element(graph, id, type) do
-        with type_plural <- :"#{type}s" do
-          graph |> Map.get(type_plural) |> Map.get(id)
+      def handle_call({:delete, id, type}, _from, {state_pid, {name, graph}}) do
+        {res, new_graph} = case kind_of_element(graph, id) do
+          ^type -> {:ok, remove(type, id, graph)}
+          _ -> {{:error, {:enotfound, type}}, graph}
         end
+        {:reply, res, {state_pid, {name, new_graph}}}
       end
 
-      defp kind_of_element(graph, id) do
-        graph |> find_element(id) |> kind_of
-      end
-
-      defp kind_of(nil), do: nil
-      defp kind_of(%{node_ids: _}), do: :cluster
-      defp kind_of(%{start_node: _}), do: :edge
-      defp kind_of(_), do: :node
-
-      defp remove_from_map!(map, id) do
-        with {_, results} <- Map.pop(map, id) do
-          results
+      def handle_cast({:update, type, id, attrs}, {state_pid, {name, graph}}) do
+        new_graph = case find_element(graph, id, type) do
+          nil -> graph
+          _ ->
+            case kind_of_element(graph, id) do
+              :node -> update_attrs_for_element_in_graph(graph, id, Map.get(graph.nodes, id), :nodes, attrs)
+              :edge -> update_attrs_for_element_in_graph(graph, id, Map.get(graph.edges, id), :edges, attrs)
+              _ -> graph
+            end
         end
+        {:noreply, {state_pid, {name, new_graph}}}
+      end
+      def handle_cast({:new, name}, {state_pid, _}) do
+        new_graph = Graphvix.State.new_graph(state_pid, name)
+        {:noreply, {state_pid, new_graph}}
+      end
+      def handle_cast(:clear, {state_pid, _}) do
+        Graphvix.State.clear(state_pid)
+        {:noreply, {state_pid, nil}}
+      end
+      def handle_cast({:switch, name}, {state_pid, {current_name, current_graph}}) do
+        Graphvix.State.save(state_pid, current_name, current_graph)
+        new_graph = Graphvix.State.load(state_pid, name)
+        {:noreply, {state_pid, new_graph}}
       end
 
-      defp remove(:node, id, graph) do
-        graph
-        |> remove_edges_attached_to_node(id)
-        |> remove_node_from_clusters(id)
-        |> update_graph_with_element_removed(:nodes, id)
-      end
-      defp remove(:edge, id, graph) do
-        update_graph_with_element_removed(graph, :edges, id)
-      end
-      defp remove(:cluster, id, graph) do
-        update_graph_with_element_removed(graph, :clusters, id)
-      end
-
-      defp get_id do
-        Graphvix.IdAgent.next
+      def terminate(_reason, {state_pid, {current_name, current_graph}}) do
+        Graphvix.State.save(state_pid, current_name, current_graph)
       end
     end
   end
--- lib/graphvix/graph.ex
+++ lib/graphvix/graph.ex
@@ -1,5 +1,68 @@
 defmodule Graphvix.Graph do
+  @moduledoc """
+  `Graphvix.Graph` manages saving, loading, and presenting the state
+  of a graph.
+
+  NB. All examples below assume you have run
+
+      iex> alias Graphvix.{Graph, Node, Edge, Cluster}
+
+  To reduce user effort, the module keeps only a single graph in state
+  at any given time. Graphs can be saved and reloaded to switch between working
+  with several different graphs at a time.
+
+      iex> Graph.new(:first_graph)
+
+      iex> ... # Add some data to the first graph
+
+      iex> Graph.switch(:second_graph) # Creates a new graph and loads it, saving the old graph at the same time.
+
+      iex> ... # Add some data to the second graph
+
+      iex> Graph.switch(:first_graph) # Saves `:second_graph` and reloads `first_graph`
+
+  Elements are added and removed from the graph using the `Node`, `Edge`,
+  and `Cluster` modules.
+
+      iex> {n_id, node} = Node.new(label: "Start", color: "blue")
+      iex> {n2_id, node2} = Node.new(label: "End", color: "red")
+      iex> {e_id, edge} = Edge.new(n_id, n2_id, color: "green")
+      iex> {c_id, cluster} = Cluster.new([n_id, n2_id])
+
+  Settings can be added to an element. Setting an attribute's value to `nil`
+  will remove the attribute.
+
+      iex> Node.update(n_id, shape: "triangle")
+      iex> Node.update(n2_id, color: nil) # Removes the key `color` from the node`s attributes keyword map
+
+  A cluster's contents can be updated using `add` and `remove`
+
+      iex> {n3_id, node3} = Node.new(label: "Something else")
+      iex> Cluster.add(c_id, n3_id)
+      iex> Cluster.remove(c_id, n_id)
+
+  In addition to saving the graph to a text file, it can be rendered in .dot
+  format and saved
+
+      iex> Graph.save(:dot) # Saves the current state of the graph as a .dot file
+
+  These files can then be compiled to .pdf/.png/etc at the command line,
+  or via additional helper functions on the `Graph` module
+
+      iex> Graph.compile # Generates files "G.dot" and "G.pdf"
+      iex> Graph.compile(:png) # Generates files "G.dot" and "G.png"
+      iex> Graph.compile("my_graph") # Generates files "my_graph.dot" and "my_graph.pdf"
+      iex> Graph.compile("my_graph", :png) # Generates files "my_graph.dot" and "my_graph.png"
+
+  To immediately view the current state of a graph, there is `Graph.graph`
+
+      iex> Graph.graph # Generates "G.dot" and "G.pdf", and opens "G.pdf" in your OS's default viewer
+      iex> Graph.graph(:png) # Same as above, but generates and opens a .png file
+      iex> Graph.graph("my_graph", :png) Same as above, but generates and opens files named "my_graph"
+
+  """
   use GenServer
+  use Graphvix.Callbacks
 
   @doc false
   def start_link(state_pid) do
@@ -52,195 +115,5 @@ defmodule Graphvix.Graph do
     GenServer.cast(__MODULE__, :clear)
   end
 
-  ## CALLBACKS
-
-  def init({state_pid, _}) do
-    new_state = {state_pid, Graphvix.State.current_graph(state_pid)}
-    schedule_save()
-    {:ok, new_state}
-  end
-
-  def handle_call(:current_graph, _from, state={_, graph}) do
-    {:reply, graph, state}
-  end
-  def handle_call(:ls, _from, state={state_pid, _}) do
-    graph_names = Graphvix.State.ls(state_pid)
-    {:reply, graph_names, state}
-  end
-  def handle_call({:add_node, attrs}, _from, state={state_pid, graph}) do
-    case graph do
-      nil ->
-        {:reply, {:error, :enograph}, state}
-      {name, graph} ->
-        id = get_id
-        new_node = %{ attrs: attrs }
-        new_nodes = Map.put(graph.nodes, id, new_node)
-        new_graph = %{ graph | nodes: new_nodes }
-        {:reply, {id, new_node}, {state_pid, {name, new_graph}}}
-    end
-  end
-  def handle_call({:add_edge, start_id, end_id, attrs}, _from, {state_pid, {name, graph}}) do
-    id = get_id
-    new_edge = %{ start_node: start_id, end_node: end_id, attrs: attrs }
-    new_edges = Map.put(graph.edges, id, new_edge)
-    new_graph = %{ graph | edges: new_edges }
-    {:reply, {id, new_edge}, {state_pid, {name, new_graph}}}
-  end
-  def handle_call({:add_cluster, node_ids}, _from, {state_pid, {name, graph}}) do
-    id = get_id
-    new_cluster = %{ node_ids: node_ids }
-    new_clusters = Map.put(graph.clusters, id, new_cluster)
-    new_graph = %{ graph | clusters: new_clusters }
-    {:reply, {id, new_cluster}, {state_pid, {name, new_graph}}}
-  end
-  def handle_call({:add_to_cluster, cluster_id, node_ids}, _from, {state_pid, {name, graph}}) do
-    cluster = Map.get(graph.clusters, cluster_id)
-    new_node_ids = (cluster.node_ids ++ node_ids) |> Enum.uniq
-    new_cluster = %{ cluster | node_ids: new_node_ids }
-    new_clusters = %{ graph.clusters | cluster_id => new_cluster }
-    {:reply, new_cluster, {state_pid, {name, %{ graph | clusters: new_clusters }}}}
-  end
-  def handle_call({:remove_from_cluster, cluster_id, node_ids}, _from, {state_pid, {name, graph}}) do
-    cluster = Map.get(graph.clusters, cluster_id)
-    new_node_ids = (cluster.node_ids -- node_ids) |> Enum.uniq
-    new_cluster = %{ cluster | node_ids: new_node_ids }
-    new_clusters = %{ graph.clusters | cluster_id => new_cluster }
-    {:reply, new_cluster, {state_pid, {name, %{ graph | clusters: new_clusters }}}}
-  end
-  def handle_call({:find, id, type}, _from, state={_, {_name, graph}}) do
-    res = case find_element(graph, id, type) do
-      nil -> {:error, {:enotfound, type}}
-      element -> element
-    end
-    {:reply, res, state}
-  end
-  def handle_call({:delete, id, type}, _from, {state_pid, {name, graph}}) do
-    {res, new_graph} = case kind_of_element(graph, id) do
-      ^type -> {:ok, remove(type, id, graph)}
-      _ -> {{:error, {:enotfound, type}}, graph}
-    end
-    {:reply, res, {state_pid, {name, new_graph}}}
-  end
-
-  def handle_cast({:update, type, id, attrs}, {state_pid, {name, graph}}) do
-    new_graph = case find_element(graph, id, type) do
-      nil -> graph
-      _ ->
-        case kind_of_element(graph, id) do
-          :node -> update_attrs_for_element_in_graph(graph, id, Map.get(graph.nodes, id), :nodes, attrs)
-          :edge -> update_attrs_for_element_in_graph(graph, id, Map.get(graph.edges, id), :edges, attrs)
-          _ -> graph
-        end
-    end
-    {:noreply, {state_pid, {name, new_graph}}}
-  end
-  def handle_cast({:new, name}, {state_pid, _}) do
-    new_graph = Graphvix.State.new_graph(state_pid, name)
-    {:noreply, {state_pid, new_graph}}
-  end
-  def handle_cast(:clear, {state_pid, _}) do
-    Graphvix.State.clear(state_pid)
-    {:noreply, {state_pid, nil}}
-  end
-  def handle_cast({:switch, name}, {state_pid, {current_name, current_graph}}) do
-    Graphvix.State.save(state_pid, current_name, current_graph)
-    new_graph = Graphvix.State.load(state_pid, name)
-    {:noreply, {state_pid, new_graph}}
-  end
-
-  def terminate(_reason, {state_pid, {current_name, current_graph}}) do
-    Graphvix.State.save(state_pid, current_name, current_graph)
-  end
-
-  defp get_id do
-    Graphvix.IdAgent.next
-  end
-
-  defp find_element(graph, id) do
-    Map.get(graph.nodes, id) || Map.get(graph.edges, id) || Map.get(graph.clusters, id)
-  end
-  defp find_element(graph, id, type) do
-    with type_plural <- :"#{type}s" do
-      graph |> Map.get(type_plural) |> Map.get(id)
-    end
-  end
-
-  defp kind_of_element(graph, id) do
-    graph |> find_element(id) |> kind_of
-  end
-
-  defp kind_of(nil), do: nil
-  defp kind_of(%{node_ids: _}), do: :cluster
-  defp kind_of(%{start_node: _}), do: :edge
-  defp kind_of(_), do: :node
-
-  defp update_attrs_for_element_in_graph(graph, id, element, key, attrs) do
-    new_attrs = merge_without_nils(element.attrs, attrs)
-    new_element = %{ element | attrs: new_attrs }
-    %{ graph | key => %{ Map.get(graph, key) | id => new_element } }
-  end
-
-  defp merge_without_nils(original, changes) do
-    original
-    |> Keyword.merge(changes)
-    |> reject_nils
-  end
-
-  defp reject_nils(coll) do
-    Enum.reject(coll, fn {_, v} -> is_nil(v) end)
-  end
-
-  defp remove(:node, id, graph) do
-    graph
-    |> remove_edges_attached_to_node(id)
-    |> remove_node_from_clusters(id)
-    |> update_graph_with_element_removed(:nodes, id)
-  end
-  defp remove(:edge, id, graph) do
-    update_graph_with_element_removed(graph, :edges, id)
-  end
-  defp remove(:cluster, id, graph) do
-    update_graph_with_element_removed(graph, :clusters, id)
-  end
-
-  defp update_graph_with_element_removed(graph, key, id) do
-    with with_removed <- remove_from_map!(Map.get(graph, key), id) do
-      %{ graph | key => with_removed }
-    end
-  end
-
-  defp remove_edges_attached_to_node(graph, node_id) do
-    new_edges = graph.edges
-                |> Enum.reject(&edge_connects_to_node?(&1, node_id))
-                |> Enum.into(Map.new)
-    %{ graph | edges: new_edges }
-  end
-
-  defp edge_connects_to_node?({_, %{ start_node: s_id, end_node: e_id }}, n_id) do
-    s_id == n_id || e_id == n_id
-  end
-
-  defp remove_node_from_clusters(graph, node_id) do
-    new_clusters = Enum.map(graph.clusters, fn {id, cluster} ->
-      {id, %{ cluster | node_ids: cluster.node_ids -- [node_id] }}
-    end) |> Enum.into(Map.new)
-   %{ graph | clusters: new_clusters }
-  end
-
-  defp remove_from_map!(map, id) do
-    with {_, results} <- Map.pop(map, id) do
-      results
-    end
-  end
-
-  def handle_info(:save_state, state={state_pid, {name, graph}}) do
-    Graphvix.State.save(state_pid, name, graph)
-    schedule_save
-    {:noreply, state}
-  end
-
-  defp schedule_save do
-    Process.send_after(self(), :save_state, 60_000)
-  end
 end
 
