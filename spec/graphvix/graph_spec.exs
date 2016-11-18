6f8606e471773371f88a612e269a1c24d4c70ba7
Michael Berkowitz <michael.berkowitz@gmail.com>
2016-11-17 23:37:47 -0500

    Rename spec file

--- spec/graphvix/graph_spec.exs
+++ spec/graphvix/graph_spec.exs
@@ -1,139 +1,160 @@
-#defmodule Graphvix.GraphSpec do
-  #use ESpec
+defmodule Graphvix.GraphSpec do
+  use ESpec
+  alias Graphvix.{Graph, Node, Edge, Cluster}
 
-  #alias Graphvix.{Graph, Node, Edge, Cluster}
+  before do
+    Graph.clear
+  end
 
-  #describe ".update" do
-    #it "sets graph-wide settings" do
-      #Graph.restart
-      #Graph.update(size: "4, 4")
-      #expect Graph.write |> to(eq """
-#digraph G {
-  #size="4, 4";
-#}
-#""" |> String.strip)
+  finally do
+    File.rm("graphvix.store")
+  end
+
+  describe ".ls" do
+    it "returns a list of graphs loaded in the state" do
+      expect Graph.ls |> to(be_empty)
+
+      Graph.new(:first)
+
+      expect Graph.ls |> to(eq [:first])
+    end
+  end
+
+  describe ".new" do
+    it "creates a new graph in the state" do
+      Graph.new(:first)
+
+      expect Graph.ls |> to(eq [:first])
+    end
+
+    it "sets the current graph" do
+      Graph.new(:first)
+
+      expect Graph.current_graph |> to(eq :first)
+    end
+  end
+
+  describe ".switch" do
+    it "switches to a different, possibly new, named graph" do
+      Graph.new(:first)
+
+      Graph.switch(:second)
+
+      expect Graph.ls |> to(eq [:first, :second])
+      expect Graph.current_graph |> to(eq :second)
+    end
+  end
+
+  describe ".update" do
+    it "sets graph-wide settings" do
+      Graph.new(:first)
+      Graph.update(size: "4, 4")
+      expect Graph.write |> to(eq """
+digraph G {
+  size="4, 4";
+}
+""" |> String.strip)
+    end
+  end
+
+  describe ".save" do
+    it "saves to DOT format" do
+      Graph.new(:first)
+      {n1_id, _n1} = Node.new(label: "Start")
+      {n2_id, _n2} = Node.new(label: "End")
+      Edge.new(n1_id, n2_id)
+      Graph.save
+
+      :timer.sleep(125)
+      {:ok, file_content} = File.read("first.dot")
+
+      expect file_content |> to(eq """
+digraph G {
+  node_#{n1_id} [label="Start"];
+  node_#{n2_id} [label="End"];
+
+  node_#{n1_id} -> node_#{n2_id};
+}
+""" |> String.strip)
+
+      :timer.sleep(125)
+      expect File.rm("first.dot") |> to(eq :ok)
+    end
+  end
+
+  describe ".write" do
+    it "returns a dot representation of the graph" do
+      Graph.new(:test)
+      expect Graph.write |> to(eq "digraph G {\n\n}")
+    end
+
+    it "returns nodes and edges correctly" do
+      Graph.new(:test)
+      {n1_id, _n1} = Node.new(label: "Start")
+      {n2_id, _n2} = Node.new(label: "End")
+      {n3_id, _n3} = Node.new(label: "Epilogue")
+      _e1 = Edge.new(n1_id, n2_id)
+      _e2 = Edge.new(n1_id, n3_id)
+
+      {c_id, _c} = Cluster.new([n1_id, n2_id])
+
+      expect Graph.write |> to(
+      eq ~s/
+digraph G {
+  node_#{n1_id} [label="Start"];
+  node_#{n2_id} [label="End"];
+  node_#{n3_id} [label="Epilogue"];
+
+  node_#{n1_id} -> node_#{n2_id};
+  node_#{n1_id} -> node_#{n3_id};
+
+  subgraph cluster_#{c_id} {
+    node_#{n1_id} -> node_#{n2_id} [style=invis];
+    { rank = "same"; node_#{n1_id}; node_#{n2_id}; }
+  }
+}/ |> String.strip)
+    end
+  end
+
+  describe ".compile" do
+    it "saves at a default file location and compiles to PDF" do
+      Graph.new(:first)
+
+      Graph.compile
+
+      :timer.sleep 125
+
+      expect File.rm("first.dot") |> to(eq :ok)
+      expect File.rm("first.pdf") |> to(eq :ok)
+    end
+
+    it "saves at a default file location and compiles to PNG if that option is passed in" do
+      Graph.new(:first)
+
+      Graph.compile(:png)
+
+      :timer.sleep 125
+
+      expect File.rm("first.dot") |> to(eq :ok)
+      expect File.rm("first.png") |> to(eq :ok)
+      expect File.rm("first.pdf") |> to(eq {:error, :enoent})
+    end
+  end
+
+  #describe "reloading state on restart" do
+    #it "should return the existing state/current graph" do
+      ## TODO: call/cast to non-existent handler to trigger the termination
+      #Graph.new(:first)
+
+      #Graph.switch(:second)
+
+      #GenServer.cast(Graph, :bad_call)
+
+      #:timer.sleep 10
+
+      #{graph_name, _graph} = Graph.current_graph
+      #expect graph_name |> to(eq :second)
     #end
   #end
+end
 
-  #describe ".save" do
-    #it "saves a raw Elixir representation of the graph to a txt file" do
-      #Graph.start
-      #Graph.save(:txt)
-
-      #:timer.sleep(125)
-      #expect File.rm("G.txt") |> to(eq :ok)
-    #end
-
-    #it "can take an optional filename argument" do
-      #Graph.start
-      #Graph.save(:txt, "my_graph")
-
-      #:timer.sleep(125)
-      #expect File.rm("G.txt") |> to(eq {:error, :enoent})
-      #expect File.rm("my_graph.txt") |> to(eq :ok)
-    #end
-
-    #it "saves to DOT format" do
-      #Graph.restart
-      #{n1_id, _n1} = Node.new(label: "Start")
-      #{n2_id, _n2} = Node.new(label: "End")
-      #Edge.new(n1_id, n2_id)
-      #Graph.save(:dot)
-
-      #:timer.sleep(125)
-      #{:ok, file_content} = File.read("G.dot")
-      #expect file_content |> to(eq """
-#digraph G {
-  #node_#{n1_id} [label="Start"];
-  #node_#{n2_id} [label="End"];
-
-  #node_#{n1_id} -> node_#{n2_id};
-#}
-#""" |> String.strip)
-
-      #:timer.sleep(125)
-      #expect File.rm("G.dot") |> to(eq :ok)
-    #end
-  #end
-
-  #describe ".load" do
-    #it "sets the current graph to the state loaded from a file" do
-      #File.write("test.txt", "%{nodes: %{1 => %{id: 1, attrs: [label: \"Hello\"]}}, edges: %{}, clusters: %{}}")
-
-      #Graph.start
-      #Graph.load("test.txt")
-
-      #expect Node.find(1) |> Map.get(:attrs) |> Keyword.get(:label) |> to(eq "Hello")
-      #expect File.rm("test.txt") |> to(eq :ok)
-    #end
-  #end
-
-  #describe ".write" do
-    #it "returns a dot representation of the graph" do
-      #Graph.restart
-      #expect Graph.write |> to(eq "digraph G {\n\n}")
-    #end
-
-    #it "returns nodes and edges correctly" do
-      #Graph.restart
-      #{n1_id, _n1} = Node.new(label: "Start")
-      #{n2_id, _n2} = Node.new(label: "End")
-      #{n3_id, _n3} = Node.new(label: "Epilogue")
-      #_e1 = Edge.new(n1_id, n2_id)
-      #_e2 = Edge.new(n1_id, n3_id)
-
-      #{c_id, _c} = Cluster.new([n1_id, n2_id])
-
-      #expect Graph.write |> to(
-      #eq ~s/
-#digraph G {
-  #node_#{n1_id} [label="Start"];
-  #node_#{n2_id} [label="End"];
-  #node_#{n3_id} [label="Epilogue"];
-
-  #node_#{n1_id} -> node_#{n2_id};
-  #node_#{n1_id} -> node_#{n3_id};
-
-  #subgraph cluster_#{c_id} {
-    #node_#{n1_id} -> node_#{n2_id} [style=invis];
-    #{ rank = "same"; node_#{n1_id}; node_#{n2_id}; }
-  #}
-#}/ |> String.strip)
-    #end
-  #end
-
-  #describe ".compile" do
-    #it "saves at a default file location and compiles to PDF" do
-      #Graph.restart
-
-      #Graph.compile
-
-      #expect File.rm("G.dot") |> to(eq :ok)
-      #expect File.rm("G.pdf") |> to(eq :ok)
-    #end
-
-    #it "saves at a default file location and compiles to PNG if that option is passed in" do
-      #Graph.restart
-
-      #Graph.compile("my_graph", :png)
-
-      #:timer.sleep 125
-
-      #expect File.rm("my_graph.dot") |> to(eq :ok)
-      #expect File.rm("my_graph.png") |> to(eq :ok)
-      #expect File.rm("my_graph.pdf") |> to(eq {:error, :enoent})
-    #end
-
-    #it "allows default filename and format as the first parameter" do
-      #Graph.restart
-
-      #Graph.compile(:png)
-
-      #:timer.sleep 125
-
-      #expect File.rm("G.dot") |> to(eq :ok)
-      #expect File.rm("G.png") |> to(eq :ok)
-      #expect File.rm("G.pdf") |> to(eq {:error, :enoent})
-    #end
-  #end
-#end
