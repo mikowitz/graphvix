0ba995d2101cf61f8e2e09ef9209c6e21a3fee28
Eduardo Aguilera <laloao.2302@gmail.com>
2018-02-14 17:25:10 -0600

    Restart id agent after graph clear #14

--- lib/graphvix/id_agent.ex
+++ lib/graphvix/id_agent.ex
@@ -5,6 +5,11 @@ defmodule Graphvix.IdAgent do
     Agent.get_and_update(agent, &{&1, &1 + 1})
   end
 
+  def clear do
+    Agent.stop(agent)
+    agent()
+  end
+
   defp agent do
     case Agent.start(fn -> 1 end, name: :id_agent) do
       {:ok, agent} -> agent
